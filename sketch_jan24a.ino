f2d33a8d82892fbac27e0620ccdab133b14e65d7
Alessio Carusio <alessiocarusio@gmail.com>
2023-01-25 22:02:49 +0100

    first commit

---
+++ sketch_jan24a.ino
@@ -0,0 +1,126 @@
+#include <DHT.h>
+
+
+#define INCREASE_BUTTON_PIN 26
+#define DECREASE_BUTTON_PIN 25
+#define RELAY_PIN 32
+#define LED_PIN 34
+#define DHT_PIN 33
+
+
+const int target_temp = 20;
+
+
+struct Button {
+  const uint8_t PIN;
+  bool pressed;
+};
+
+struct Thermostat {
+  int target_temp;
+};
+
+hw_timer_t *My_timer = NULL;
+
+DHT dht(DHT_PIN, DHT22);
+Button increaseButton = { INCREASE_BUTTON_PIN, false };
+Button decreaseButton = { DECREASE_BUTTON_PIN, false };
+Thermostat thermostat = { target_temp };
+
+
+//variables to keep track of the timing of recent interrupts
+unsigned long button_time = 0;
+unsigned long last_button_time = 0;
+
+bool expiredTimer = false;
+float prova = 0;
+// -------------------------- ISR ------------------------
+void IRAM_ATTR onTimer() {
+  expiredTimer = true;
+}
+
+
+void IRAM_ATTR increaseISR() {
+  button_time = millis();
+  if (button_time - last_button_time > 250) {
+    if (thermostat.target_temp < 30) {
+      thermostat.target_temp++;
+      increaseButton.pressed = true;
+      last_button_time = button_time;
+      Serial.print("Target Temperature: ");
+      Serial.println(thermostat.target_temp);
+    } else {
+      increaseButton.pressed = true;
+      last_button_time = button_time;
+      Serial.println("Temperatura massima target raggiunta");
+    }
+  }
+}
+
+
+void IRAM_ATTR decreaseISR() {
+  button_time = millis();
+  if (button_time - last_button_time > 250) {
+    if (thermostat.target_temp > 10) {
+      decreaseButton.pressed = true;
+      last_button_time = button_time;
+      thermostat.target_temp--;
+      Serial.print("Target Temperature: ");
+      Serial.println(thermostat.target_temp);
+
+    } else {
+      decreaseButton.pressed = true;
+      last_button_time = button_time;
+      Serial.println("Temperatura minima target raggiunta");
+    }
+  }
+}
+
+// -------------------------- SETUP ------------------------
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(increaseButton.PIN, INPUT_PULLUP);
+  pinMode(decreaseButton.PIN, INPUT_PULLUP);
+  attachInterrupt(increaseButton.PIN, increaseISR, RISING);
+  attachInterrupt(decreaseButton.PIN, decreaseISR, RISING);
+
+  pinMode(RELAY_PIN, OUTPUT);
+
+  dht.begin();
+
+  // Leggi qua per info sul clock e sul prescaler https://circuitdigest.com/microcontroller-projects/esp32-timers-and-timer-interrupts
+  My_timer = timerBegin(0, 9600, true); //2 minuti
+  timerAttachInterrupt(My_timer, &onTimer, true);
+  timerAlarmWrite(My_timer, 1000000, true);
+  timerAlarmEnable(My_timer);  //Just Enable
+}
+
+
+
+// -------------------------- LOOP ------------------------
+
+void loop() {
+
+
+  //By the way, an interrupt function must be as small as possible :
+  //you should try rewriting it just for toggling a flag. Then
+  //you check the flag in your loop and do the rest of the current ISR tasks if the flag is ON.
+
+  if (expiredTimer) {
+    //UPDATE DISPLAY INFO
+    Serial.println(dht.readTemperature());
+
+    if (thermostat.target_temp > dht.readTemperature()) {
+      
+      digitalWrite(RELAY_PIN, HIGH);
+      //  digitalWrite(led, HIGH);
+
+    } else {
+      digitalWrite(RELAY_PIN, LOW);
+      //  digitalWrite(led, LOW);
+    }
+    
+    expiredTimer = false;
+  }
+}
